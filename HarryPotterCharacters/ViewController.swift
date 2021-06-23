379897e8f6506e21c8bd0f0cfea2a5c81fa9f0fa
Sean Startin <seanstartin92@hotmail.co.uk>
2021-06-23 08:49:37 +0100

    Initial Commit

---
+++ HarryPotterCharacters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HarryPotterCharacters
+//
+//  Created by Sean Startin on 23/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
